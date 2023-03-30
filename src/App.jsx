a4646bff75e758ee115df615aba5fcc961b3285d
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-03-30 18:00:19 -0300

    create player

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Video from './pages/Video';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
+}
+
+export default App;
